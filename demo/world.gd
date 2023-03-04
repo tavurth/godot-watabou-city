ff31aa1590d27f0b357e52dfd71fdbfaeab45e15
Will Whitty <tavurth@gmail.com>
2023-03-04 21:09:32 +0700

    Reload map in a nicer way

--- demo/world.gd
+++ demo/world.gd
@@ -1,7 +1,12 @@
 @tool
 extends Node2D
 
-@export var map: WatabouMap = WatabouMap.new()
+@export var map: WatabouMap = WatabouMap.new():
+	set = _set_map
 
-func _ready():
+
+func _set_map(new_map: WatabouMap) -> void:
+	for child in self.get_children():
+		self.remove_child(child)
+
 	self.map.draw(self)
