07d7f337f374eb3b75b741a51009b6d83403cd2c
Will Whitty <tavurth@gmail.com>
2023-03-05 01:23:21 +0700

    Nicer smoothing

--- addons/godot-watabou-city/Helpers.gd
+++ addons/godot-watabou-city/Helpers.gd
@@ -1,6 +1,6 @@
 extends RefCounted
 
-static func smooth_line2d(line: Line2D, num_points: int = 10) -> void:
+static func smooth_line2d(line: Line2D, num_points: int = 2, corner_speed: float = 4) -> void:
 	var curve := Curve2D.new()
 
 	for idx in range(len(line.points)):
@@ -8,9 +8,10 @@ static func smooth_line2d(line: Line2D, num_points: int = 10) -> void:
 
 		if idx > 0:
 			var delta = line.points[idx] - line.points[idx-1]
+			delta /= corner_speed
 
-			curve.set_point_in(idx, delta / -2)
-			curve.set_point_out(idx, delta / 2)
+			curve.set_point_in(idx, -delta)
+			curve.set_point_out(idx, +delta)
 
 	var points = curve.tessellate(num_points)
 
